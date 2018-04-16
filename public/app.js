286f408970b7a6e0d287b264807e24f3cc85aea4
Lizaveta <l.manzhulina@gmail.com>
2018-04-16 19:24:12 +0300

    add public

---
+++ public/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'))// путь к public
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.use((req, res) => {
+    res.send("oops, an error");
+})
