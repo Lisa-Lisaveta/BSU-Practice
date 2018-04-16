07edc1253bc3afc849db182f8262efb3b6dc699d
Lizaveta <l.manzhulina@gmail.com>
2018-04-16 19:19:52 +0300

    add app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var app = express();
+
+app.use(express.static('public'))
+
+
+app.listen(1984, function () {
+  console.log('listening on port 1984');
+});
+
+app.use((req, res) => {
+    res.send("oops, an error occured");
+})
